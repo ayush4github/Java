a139a8d1d28dcc0a85a6a7dd1dde9c29eea8d2c0
ayush4github <ayushplays712@gmail.com>
2025-10-07 17:59:50 +0530

    Initial commit

---
+++ Methods/InnerClass.java
@@ -0,0 +1,13 @@
+class OuterClass{
+    int x=10;
+    class InnerClass{
+        int y=10;
+    }
+}
+public class InnerClass {
+    public static void main(String[] args) {
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println(myInner.y + myOuter.x);
+    }
+}
