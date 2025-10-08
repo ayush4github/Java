3e7515337c2ed48fd7e859f581674f3220ea4bdf
ayush4github <ayushplays712@gmail.com>
2025-10-08 22:57:11 +0530

    Initial Commit

---
+++ Classes/Enum1.java
@@ -0,0 +1,15 @@
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+public class Enum1 {
+    public static void main(String[] args) {
+        Level Level1 = Level.LOW;
+        Level Level2 = Level.MEDIUM;
+        Level Level3 = Level.HIGH;
+        System.out.println("Level1: "+ Level1);
+        System.out.println("Level2: "+Level2);
+        System.out.println("Level3: "+Level3);
+    }
+}
