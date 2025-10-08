9752b9c32e9d2d891a5cbea37e295eb7b17f0783
ayush4github <ayushplays712@gmail.com>
2025-10-09 00:24:04 +0530

    Initial commit

---
+++ Classes/CurrentDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class CurrentDate {
+ public static void main(String[] args) {
+    LocalDate Datenow = LocalDate.now();
+    System.out.println(Datenow);
+ }   
+}
