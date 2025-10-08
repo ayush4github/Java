2563601995a00eab3aaa36b971553aad6ebbc6d5
ayush4github <ayushplays712@gmail.com>
2025-10-08 22:59:24 +0530

    Initial commit

---
+++ Classes/EnumInsideClass.java
@@ -0,0 +1,11 @@
+public class EnumInsideClass {
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
