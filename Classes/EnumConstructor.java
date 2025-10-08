738303a1f03f58da2620d90086ed8112287af1e6
ayush4github <ayushplays712@gmail.com>
2025-10-08 23:27:44 +0530

    Initial commit

---
+++ Classes/EnumConstructor.java
@@ -0,0 +1,18 @@
+enum Level{
+    LOW("Low Level"),
+    MEDIUM("Medium Level"),
+    HIGH("High Level");
+    private String description;
+    private Level(String description){
+        this.description=description;
+    }
+    public String getdescription(){
+        return description;
+    }
+}
+public class EnumConstructor {
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar.getdescription());
+    }
+}
