9efb3e621a5503a9403e2c8ed8388bcee0a748d0
ayush4github <ayushplays712@gmail.com>
2025-10-09 01:02:24 +0530

    Initial commit

---
+++ Errors/PrintStatementForDebugging.java
@@ -0,0 +1,9 @@
+public class PrintStatementForDebugging {
+    public static void main(String[] args) {
+        int x=10;
+    int y =0;
+    System.out.println("Before division");
+    int result = x/y;
+    System.out.println("Result: "+ result);
+    }
+}
