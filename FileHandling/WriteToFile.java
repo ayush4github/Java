2f5393813d28c192ff17b03760a146f72add45a1
ayush4github <ayushplays712@gmail.com>
2025-10-09 14:30:27 +0530

    Initial commit

---
+++ FileHandling/WriteToFile.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+public class WriteToFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter myWriter = new FileWriter("filename2.txt");
+            myWriter.write("Files in java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        }
+        catch(IOException e){
+            System.out.println("An error occured.");
+        }
+    }
+}
