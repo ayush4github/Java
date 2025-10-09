b043a2579410865543e5ed28a84ea3a4433872e1
ayush4github <ayushplays712@gmail.com>
2025-10-09 14:20:18 +0530

    Initial commit

---
+++ FileHandling/CreateFile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+public class CreateFile {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()){
+                System.out.println("File Created: "+myObj.getName());
+            }
+            else{
+                System.out.println("File already exists");
+            }
+        }
+        catch(IOException e){
+            System.out.println("An error occurend");
+            e.printStackTrace();
+        }
+    }
+}
