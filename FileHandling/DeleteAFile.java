4856280e60bb40ff387db7edd0eef199e3fe7f97
ayush4github <ayushplays712@gmail.com>
2025-10-09 16:34:51 +0530

    Initial commit

---
+++ FileHandling/DeleteAFile.java
@@ -0,0 +1,13 @@
+//package FileHandling;
+import java.io.File;
+public class DeleteAFile {
+    public static void main(String[] args) {
+        File myObj = new File("filename.txt");
+        if (myObj.delete()){
+            System.out.println("Deleted the file: "+myObj.getName());
+        }
+        else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
+}
