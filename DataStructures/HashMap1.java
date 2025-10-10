4074bca4f6e2a54e9d1801cf5bb18c6c93ed103f
ayush4github <ayushplays712@gmail.com>
2025-10-11 00:52:42 +0530

    Initial commit

---
+++ DataStructures/HashMap1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class HashMap1{
+    public static void main(String[] args) {
+        HashMap <String, String> CapitalCities = new HashMap<String, String>();
+        CapitalCities.put("England ", " London");
+        CapitalCities.put("Germany ", " Berlin");
+        CapitalCities.put("India ", " New Delhi");
+        CapitalCities.put("Norway ", " Oslo");
+        CapitalCities.put("USA ", " Washington DC");
+        System.out.println(CapitalCities);
+    }
+}
