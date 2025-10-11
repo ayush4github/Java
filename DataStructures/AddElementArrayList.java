8a9ac17df61dcba37030d85fddc939001c00bf3d
ayush4github <ayushplays712@gmail.com>
2025-10-11 13:03:53 +0530

    Initial commit

---
+++ DataStructures/AddElementArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class AddElementArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add(0, "Mazda");
+        System.out.println(cars);
+    }
+}
