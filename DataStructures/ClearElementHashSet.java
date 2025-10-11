549bf809e6614020d460dee0eb95a0f90822bab9
ayush4github <ayushplays712@gmail.com>
2025-10-11 16:25:52 +0530

    Initial commit

---
+++ DataStructures/ClearElementHashSet.java
@@ -0,0 +1,16 @@
+    import java.util.HashSet;
+    public class ClearElementHashSet{
+        public static void main(String[] args) {
+            HashSet<String> cars = new HashSet<String>();
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("BMW");
+            cars.add("Mazda");
+            System.out.println(cars);
+            boolean x = cars.contains("Volvo");
+            System.out.println(x);
+            cars.clear();
+            System.out.println(cars);
+        }
+    }
