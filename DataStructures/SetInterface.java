a56dfa0dd0556e45551dc671a430f4bab82a33b1
ayush4github <ayushplays712@gmail.com>
2025-10-11 16:50:14 +0530

    Initial commit

---
+++ DataStructures/SetInterface.java
@@ -0,0 +1,12 @@
+import java.util.Set;
+import java.util.HashSet;
+public class SetInterface {
+    public static void main(String[] args) {
+        Set<String> cars = new HashSet<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
